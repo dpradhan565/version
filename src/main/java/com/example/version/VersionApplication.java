350e601a755c1194a3ea5119c1f0aa16e19bd46e
Dhaneshwar Pradhan <dhaneshwar.pradhan@cognizant.com>
2022-09-04 17:31:02 +0530

    second comit

--- src/main/java/com/example/version/VersionApplication.java
+++ src/main/java/com/example/version/VersionApplication.java
@@ -1,12 +1,25 @@
 package com.example.version;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VersionApplication {
 
+	
+	public static Logger logger = LoggerFactory.getLogger(VersionApplication.class);
+	
+	@PostConstruct
+	public void intt() {
+		logger.info("Application Stared!!!");
+	}
 	public static void main(String[] args) {
+		logger.info("Application Executed!!!");
+		logger.info("Application Executed!!!!!!!!!!");
 		SpringApplication.run(VersionApplication.class, args);
 	}
 
